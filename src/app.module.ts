3567fb6c8ba674b488dc96da1652740cff605d31
JamDev0 <jamdeveloper0@gmail.com>
2023-09-29 10:07:56 -0300

    feat: Switch from directly register to Import of PlayersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PlayersController } from './controllers/players.controller';
-import { PlayersService } from './services/players.service';
+import { PlayersModule } from './modules/players.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, PlayersController],
-  providers: [AppService, PlayersService],
+  imports: [PlayersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
