2169b1fa9ffbec182a2b63f6e0c92173053fe524
JamDev0 <jamdeveloper0@gmail.com>
2023-09-29 09:04:50 -0300

    feat: register of the PlayersService in app's module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayersController } from './controllers/players.controller';
+import { PlayersService } from './services/players.service';
 
 @Module({
   imports: [],
   controllers: [AppController, PlayersController],
-  providers: [AppService],
+  providers: [AppService, PlayersService],
 })
 export class AppModule {}
