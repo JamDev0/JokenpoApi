2e10c1bab90908b15190dcb33f427e629fffbcd6
JamDev0 <jamdeveloper0@gmail.com>
2023-10-03 15:20:29 -0300

    feat: implementation of the util entity and its push validation on rounds service

--- src/services/rounds.service.ts
+++ src/services/rounds.service.ts
@@ -2,10 +2,23 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { frasalArrayListing } from 'src/helpers/frasalArrayListing';
 import { isDuplicated } from 'src/helpers/isDuplicated';
 import { Round } from 'src/interfaces/round.interface';
+import { Entity } from 'src/utils/entity';
+
+function validateNewRoundEntity(pushedGames: Round[], rounds: Round[]) {
+  pushedGames.forEach((round) => {
+    const isRoundDuplicated = isDuplicated<Round>(rounds, { id: round.id });
+
+    if (isRoundDuplicated)
+      throw new HttpException(
+        `${frasalArrayListing(isRoundDuplicated)} are/is not unique!`,
+        HttpStatus.BAD_REQUEST,
+      );
+  });
+}
 
 @Injectable()
 export class RoundsService {
-  private readonly rounds: Round[] = [];
+  private readonly rounds: Round[] = new Entity(validateNewRoundEntity);
 
   create(round: Round) {
     const isRoundDuplicated = isDuplicated<Round>(this.rounds, {
